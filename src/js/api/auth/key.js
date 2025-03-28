c557428c44fc3ce9bacac6d0bb2fe6a75e8615a5
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2025-03-28 14:31:06 +0100

    feat: login functionality
    - added login form
    - added login function with POST request api
    - implement login function to login form and tested error handling

---
+++ src/js/api/auth/key.js
@@ -0,0 +1,8 @@
+export async function getKey() {
+    const accessToken = localStorage.getItem('accessToken');
+    if (!accessToken) {
+        console.error("Access token not found.");
+        return null;
+    }
+    return accessToken;
+}
--- src/js/router/views/login.js
+++ src/js/router/views/login.js
@@ -1,3 +1,24 @@
-document.body.innerHTML = "<h1>Hello, this is login page</h1>";
+import { onLogin } from "../../ui/auth/login";
 
-console.log("this is login");
+const togglePassword = document.getElementById("togglePassword");
+const passwordInput = document.getElementById("password");
+
+togglePassword.addEventListener("click", () => {
+    const type = passwordInput.type === "password" ? "text" : "password";
+    passwordInput.type = type;
+
+    togglePassword.innerHTML =
+        type === "password"
+            ? "<i class=\"fas fa-eye\"></i>"
+            : "<i class=\"fas fa-eye-slash\"></i>";
+});
+
+const form = document.forms.login;
+
+/**
+ * Adds a submit event listener to the login form.
+ *
+ * When the form is submitted, the `onLogin` function is called to handle the login process.
+ */
+form.addEventListener("submit", onLogin);
+
