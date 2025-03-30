2285d2b8242edd146c0073343ad00be2a8ff2935
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2025-03-30 19:48:03 +0200

    feat: implemented pet card for admin page
    - refactored renderPetCard to have admin parameter to render admin pet card
    - the function will only fetch pets that matched logged in user name for admin page

---
+++ src/js/router/views/admin.js
@@ -0,0 +1,3 @@
+import {renderPetCardAdmin} from "../../ui/admin/display.js";
+
+renderPetCardAdmin()
--- src/js/ui/admin/display.js
+++ src/js/ui/admin/display.js
@@ -1,1 +1,33 @@
-import {FALLBACK_IMG, FALLBACK_AVATAR} from "../../api/constants.js";
+import { renderPetTemplate } from "../listing/display.js";
+import { getListings } from "../../api/listing/read.js";
+
+export async function renderPetCardAdmin() {
+    const cardContainer = document.getElementById("pet-list");
+    const loggedInUser = localStorage.getItem("userName");
+
+    try {
+        const response = await getListings();
+        const pets = response.data;
+
+        if (!pets || !loggedInUser) {
+            cardContainer.innerHTML = `<h1>No pet found</h1>`;
+            return;
+        }
+
+        const userPets = pets.filter(pet => pet.owner?.name === loggedInUser);
+
+        if (userPets.length === 0) {
+            cardContainer.innerHTML = `<h1>You haven't listed any pets yet.</h1>`;
+            return;
+        }
+
+        userPets.forEach(pet => {
+            const card = renderPetTemplate(pet, "admin");
+            cardContainer.appendChild(card);
+        });
+
+    } catch (error) {
+        console.error("Fetch failed:", error);
+        cardContainer.innerHTML = `<h1>${error.message}</h1>`;
+    }
+}
