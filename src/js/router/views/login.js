05b4ba0ca085742674dd550d17ed25f3dd5c7045
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2025-04-01 18:01:19 +0200

    refactor: togglePasswordVisibility
    - refactored it into function inside utils to be used on all password inputs

--- src/js/router/views/login.js
+++ src/js/router/views/login.js
@@ -1,24 +1,9 @@
-import { onLogin } from "../../ui/auth/login";
+import {onLogin} from "../../ui/auth/login";
+import {setupPasswordToggles} from "../../utilities/togglePasswordVisibility.js";
 
-const togglePassword = document.getElementById("togglePassword");
-const passwordInput = document.getElementById("password");
-
-togglePassword.addEventListener("click", () => {
-    const type = passwordInput.type === "password" ? "text" : "password";
-    passwordInput.type = type;
-
-    togglePassword.innerHTML =
-        type === "password"
-            ? "<i class=\"fas fa-eye\"></i>"
-            : "<i class=\"fas fa-eye-slash\"></i>";
-});
+setupPasswordToggles();
 
 const form = document.forms.login;
 
-/**
- * Adds a submit event listener to the login form.
- *
- * When the form is submitted, the `onLogin` function is called to handle the login process.
- */
 form.addEventListener("submit", onLogin);
 
